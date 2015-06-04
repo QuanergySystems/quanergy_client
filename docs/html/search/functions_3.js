b7010253b7a292a5c4b07a6f9cf8a3f259ee4d4e
Ross Taylor <ross.taylor@quanergy.com>
2015-06-04 10:02:03 -0700

    Update: docs

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['setmaximumdistancethreshold',['setMaximumDistanceThreshold',['../classM8Client.html#abc5b947f52e385374e89f873ac82d565',1,'M8Client']]],
-  ['setminimumdistancethreshold',['setMinimumDistanceThreshold',['../classM8Client.html#a9696b5d69c51eeb6d6222d1c7cf62cbb',1,'M8Client']]],
+  ['setringfilterminimumintensitythreshold',['setRingFilterMinimumIntensityThreshold',['../classM8Client.html#a3bcea5dffd7e8f2890f550fa91f3ad31',1,'M8Client']]],
+  ['setringfilterminimumintensitythresholds',['setRingFilterMinimumIntensityThresholds',['../classM8Client.html#a00ea1c0b83f0f435a898113925d38650',1,'M8Client']]],
+  ['setringfilterminimumrangethreshold',['setRingFilterMinimumRangeThreshold',['../classM8Client.html#a9ee5c3d843bde70128b79c1fbbbbbc93',1,'M8Client']]],
+  ['setringfilterminimumrangethresholds',['setRingFilterMinimumRangeThresholds',['../classM8Client.html#a737bf83a90081de7da30cad24c919748',1,'M8Client']]],
   ['start',['start',['../classM8Client.html#a57abc3cc5f1f417403ad315af858cabe',1,'M8Client']]],
   ['stop',['stop',['../classM8Client.html#a4e5c1af01541b1e00604fe1267528286',1,'M8Client']]]
 ];
