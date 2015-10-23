690f15a6d2ce218e9390eeb87dc09aaf402eff73
ttessman <thant.tessman@quanergy.com>
2015-10-23 13:35:51 -0700

    Add: Added Client::Ptr type declaration.
    Strangely, it doesn't seem to work...

--- include/quanergy/client/client.h
+++ include/quanergy/client/client.h
@@ -11,6 +11,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <thread>
+#include <memory>
 
 #include <boost/asio.hpp>
 #include <boost/signals2.hpp>
@@ -28,56 +29,59 @@ namespace quanergy
   template <class... Types>
   class Client
   {
-    public:
-      typedef typename PointCloudGenerator<Types...>::PointCloud PointCloud;
-      typedef typename PointCloudGenerator<Types...>::CloudSignal CloudSignal;
+  public:
+    
+    typedef std::shared_ptr<typename Client> Ptr;
+    typedef typename PointCloudGenerator<Types...>::PointCloud PointCloud;
+    typedef typename PointCloudGenerator<Types...>::CloudSignal CloudSignal;
 
-      /** \brief Constructor taking a host and port. */
-      Client(const std::string& host, const std::string& port);
+    /** \brief Constructor taking a host and port. */
+    Client(const std::string& host, const std::string& port);
 
-      virtual ~Client();
+    virtual ~Client();
 
-      /** \brief Connect a slot to the signal which will be emitted when a new cloud is available */
-      boost::signals2::connection connect(const typename CloudSignal::slot_type& subscriber);
+    /** \brief Connect a slot to the signal which will be emitted when a new cloud is available */
+    boost::signals2::connection connect(const typename CloudSignal::slot_type& subscriber);
 
-      /** \brief Starts processing the Quanergy packets */
-      virtual void run();
+    /** \brief Starts processing the Quanergy packets */
+    virtual void run();
 
-      /** \brief Stops processing the Quanergy packets */
-      virtual void stop();
+    /** \brief Stops processing the Quanergy packets */
+    virtual void stop();
 
-    protected:
-      /** \brief Asynchronously wait for connection */
-      virtual void startDataConnect();
-      /** \brief Asynchronously read from socket */
-      virtual void startDataRead();
-      /** \brief Handle read of packet header */
-      virtual void handleReadHeader(const boost::system::error_code& error);
-      /** \brief Handle read of packet body */
-      virtual void handleReadBody(const boost::system::error_code& error);
+  protected:
 
-      /** \brief pulls packets off buffer queue and calls toPointCloud */
-      virtual void parsePackets();
-      /** \brief converts packet to pointcloud and signals completion as needed */
-      virtual void toPointCloud(const std::vector<char>& packet);
+    /** \brief Asynchronously wait for connection */
+    virtual void startDataConnect();
+    /** \brief Asynchronously read from socket */
+    virtual void startDataRead();
+    /** \brief Handle read of packet header */
+    virtual void handleReadHeader(const boost::system::error_code& error);
+    /** \brief Handle read of packet body */
+    virtual void handleReadBody(const boost::system::error_code& error);
 
-      std::unique_ptr<boost::asio::ip::tcp::socket> read_socket_;
-      std::vector<char>                             buff_;
-      PointCloudGenerator<Types...>                 point_cloud_generator_;
+    /** \brief pulls packets off buffer queue and calls toPointCloud */
+    virtual void parsePackets();
+    /** \brief converts packet to pointcloud and signals completion as needed */
+    virtual void toPointCloud(const std::vector<char>& packet);
 
-    private:
-      boost::asio::io_service                       io_service_;
-      boost::asio::ip::tcp::resolver::query         host_query_;
+    std::unique_ptr<boost::asio::ip::tcp::socket> read_socket_;
+    std::vector<char>                             buff_;
+    PointCloudGenerator<Types...>                 point_cloud_generator_;
 
-      /// thread for running parsing
-      std::unique_ptr<std::thread>              parse_thread_;
+  private:
+    boost::asio::io_service                       io_service_;
+    boost::asio::ip::tcp::resolver::query         host_query_;
 
-      std::queue<std::shared_ptr<std::vector<char>>> buff_queue_;
-      std::mutex                    buff_queue_mutex_;
-      std::condition_variable       buff_queue_conditional_;
-      std::atomic_bool              kill_;
+    /// thread for running parsing
+    std::unique_ptr<std::thread>              parse_thread_;
 
-      std::shared_ptr<CloudSignal> cloud_signal_;
+    std::queue<std::shared_ptr<std::vector<char>>> buff_queue_;
+    std::mutex                    buff_queue_mutex_;
+    std::condition_variable       buff_queue_conditional_;
+    std::atomic_bool              kill_;
+
+    std::shared_ptr<CloudSignal> cloud_signal_;
   };
 }
 
--- include/quanergy/client/failover_client.h
+++ include/quanergy/client/failover_client.h
@@ -15,26 +15,29 @@ namespace quanergy
   template <class... Types>
   class FailoverClient : public Client<Types..., M8DataPacket>
   {
-    public:
-      /** \brief Constructor taking a host and port. */
-      FailoverClient(const std::string& host, const std::string& port);
+  public:
 
-      virtual ~FailoverClient();
+    typedef std::shared_ptr<typename FailoverClient> Ptr;
 
-    protected:
-      virtual void startDataRead();
-      virtual void handleReadHeader(const boost::system::error_code& error);
+    /** \brief Constructor taking a host and port. */
+    FailoverClient(const std::string& host, const std::string& port);
 
-      /** \brief converts packet to pointcloud and signals completion as needed */
-      virtual void toPointCloud(const std::vector<char>& packet);
+    virtual ~FailoverClient();
 
-    private:
-      /// variable for automatic packet failover to old m8 packet parsing
-      bool failover;
+  protected:
+    virtual void startDataRead();
+    virtual void handleReadHeader(const boost::system::error_code& error);
 
-      using Client<Types..., M8DataPacket>::read_socket_;
-      using Client<Types..., M8DataPacket>::buff_;
-      using Client<Types..., M8DataPacket>::point_cloud_generator_;
+    /** \brief converts packet to pointcloud and signals completion as needed */
+    virtual void toPointCloud(const std::vector<char>& packet);
+
+  private:
+    /// variable for automatic packet failover to old m8 packet parsing
+    bool failover;
+
+    using Client<Types..., M8DataPacket>::read_socket_;
+    using Client<Types..., M8DataPacket>::buff_;
+    using Client<Types..., M8DataPacket>::point_cloud_generator_;
   };
 }
 
