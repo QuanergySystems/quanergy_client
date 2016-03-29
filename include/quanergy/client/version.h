09e88dfcb17c72b9a9140bac4d70112ee61c2927
Ross Taylor <ross.taylor@quanergy.com>
2016-03-29 14:01:33 -0700

    Ops: update versions

--- include/quanergy/client/version.h
+++ include/quanergy/client/version.h
@@ -12,10 +12,10 @@
 //  QUANERGY_CLIENT_VERSION / 100 % 1000 is the minor version
 //  QUANERGY_CLIENT_VERSION / 100000 is the major version
 
-#define QUANERGY_CLIENT_VERSION 200007
+#define QUANERGY_CLIENT_VERSION 200008
 
 // As a string.
 
-#define  QUANERGY_CLIENT_LIB_VERSION "2.0.7"
+#define  QUANERGY_CLIENT_LIB_VERSION "2.0.8"
 
 #endif
