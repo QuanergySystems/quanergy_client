b0e3747081db51655e23904366d1656ff621c5cd
Ross Taylor <ross.taylor@quanergy.com>
2020-04-22 13:22:56 -0700

    Update: dynamic connection app

--- apps/dynamic_connection.cpp
+++ apps/dynamic_connection.cpp
@@ -1,6 +1,6 @@
 /****************************************************************
  **                                                            **
- **  Copyright(C) 2018 Quanergy Systems. All Rights Reserved.  **
+ **  Copyright(C) 2020 Quanergy Systems. All Rights Reserved.  **
  **  Contact: http://www.quanergy.com                          **
  **                                                            **
  ****************************************************************/
@@ -10,163 +10,155 @@
 #include <condition_variable>
 
 // console parser
-#include <pcl/console/parse.h>
+#include <boost/program_options.hpp>
 
+// TCP client for sensor
 #include <quanergy/client/sensor_client.h>
 
-// parsers for the data packets we want to support
-#include <quanergy/parsers/data_packet_parser_00.h>
-#include <quanergy/parsers/data_packet_parser_01.h>
-#include <quanergy/parsers/data_packet_parser_04.h>
-#include <quanergy/parsers/variadic_packet_parser.h>
-
-// module to apply encoder correction
-#include <quanergy/modules/encoder_angle_calibration.h>
-
-// conversion module from polar to Cartesian
-#include <quanergy/modules/polar_to_cart_converter.h>
-
-// define some strings that will be used on command line
-namespace
-{
-  static const std::string MANUAL_CORRECT{"--manual-correct"};
-  static const std::string CALIBRATE_STR{"--calibrate"};
-  static const std::string AMPLITUDE_STR{"--amplitude"};
-  static const std::string PHASE_STR{"--phase"};
-  static const std::string FRAME_RATE_STR{"--frame-rate"};
-}
-
-// output usage message
-void usage(char** argv)
-{
-  std::cout << "usage: " << argv[0]
-            << " --host <host> [-h | --help] [" << CALIBRATE_STR << "][" << FRAME_RATE_STR << " <value> ][" << MANUAL_CORRECT << " " << AMPLITUDE_STR << " <value> " << PHASE_STR << " <value>]" << std::endl << std::endl
-
-            << "    --host        hostname or IP address of the sensor" << std::endl
-            << "    " << CALIBRATE_STR << "   calibrate the host sensor and apply calibration to outgoing points" << std::endl
-            << "    " << FRAME_RATE_STR << "  frame_rate of data; used by calibration only"
-            << "    " << MANUAL_CORRECT << " --amplitude <amplitude> --phase <phase>    Manually correct encoder error specifying amplitude and phase correction, in radians" << std::endl
-            << "-h, --help        show this help and exit" << std::endl;
-  return;
-}
-
-// convenient typedefs
-// SensorClient connects to any quanergy sensor; it pulls TCP packets off the stream, queues them, and then signals them on a separate thread
-typedef quanergy::client::SensorClient ClientType;
-// VariadicPacketParser produces a polar point cloud; it uses information in the packet to find the correct parser from the ones supplied
-typedef quanergy::client::VariadicPacketParser<quanergy::PointCloudHVDIRPtr,                      // return type
-                                               quanergy::client::DataPacketParser00,              // PARSER_00_INDEX
-                                               quanergy::client::DataPacketParser01,              // PARSER_01_INDEX
-                                               quanergy::client::DataPacketParser04> ParserType;  // PARSER_04_INDEX
-
-// enum to make indexing into the VariadicPacketParser easier
-enum
-{
-  PARSER_00_INDEX = 0,
-  PARSER_01_INDEX = 1,
-  PARSER_04_INDEX = 2
-};
-
-// more typedefs
-// PacketParserModule wraps the parser with signal/slot functionality
-typedef quanergy::client::PacketParserModule<ParserType> ParserModuleType;
-// EncoderAngleCalibration provides some calibration functionality for M8
-typedef quanergy::calibration::EncoderAngleCalibration CalibrationType;
-// PolarToCartConverter converts the polar point cloud to Cartesian
-typedef quanergy::client::PolarToCartConverter ConverterType;
+// sensor pipeline
+#include <quanergy/pipelines/sensor_pipeline.h>
 
 int main(int argc, char** argv)
 {
-  int max_num_args = 8;
-  // get host
-  if (argc < 2 || argc > max_num_args || pcl::console::find_switch(argc, argv, "-h") ||
-      pcl::console::find_switch(argc, argv, "--help") || !pcl::console::find_switch(argc, argv, "--host"))
-  {
-    usage (argv);
-    return (0);
-  }
+  namespace po = boost::program_options;
 
-  std::string host;
+  po::options_description description("Quanergy Client Visualizer");
+  const po::positional_options_description p; // empty positional options
+
+  quanergy::pipeline::SensorPipelineSettings pipeline_settings;
+  std::string return_string;
+  std::vector<float> correct_params;
+
+  // port
   std::string port = "4141";
 
-  pcl::console::parse_argument(argc, argv, "--host", host);
+  description.add_options()
+    ("help,h", "Display this help message")
+    ("settings-file,s", po::value<std::string>(),
+      "Settings file. Setting file values override defaults and command line arguments override the settings file.")
+    ("host", po::value<std::string>(&pipeline_settings.host),
+      "Host name or IP of the sensor.")
+    ("frame,f", po::value<std::string>(&pipeline_settings.frame)->
+      default_value(pipeline_settings.frame),
+      "Frame name inserted in the point cloud.")
+    ("return,r", po::value<std::string>(&return_string)->
+      default_value(pipeline_settings.stringFromReturn(pipeline_settings.return_selection)),
+      "Return selection for multiple return M-series sensors. "
+      "Options are 0, 1, 2, or all. If 'all' is chosen, then all 3 returns will be in an unorganized point cloud.")
+    ("calibrate", po::bool_switch(&pipeline_settings.calibrate),
+      "Flag indicating encoder calibration should be performed and applied to outgoing points")
+    ("frame-rate", po::value<double>(&pipeline_settings.frame_rate)->
+      default_value(pipeline_settings.frame_rate),
+      "Frame rate used when peforming encoder calibration")
+    ("manual-correct", po::value<std::vector<float>>(&correct_params)->multitoken()->value_name("amplitude phase"),
+      "Correct encoder error with user defined values. Both amplitude and phase are in radians")
+    ("min-distance", po::value<float>(&pipeline_settings.min_distance)->
+      default_value(pipeline_settings.min_distance),
+      "minimum distance (inclusive) for distance filtering.")
+    ("max-distance", po::value<float>(&pipeline_settings.max_distance)->
+      default_value(pipeline_settings.max_distance),
+      "maximum distance (inclusive) for distance filtering.")
+    ("min-cloud-size", po::value<std::int32_t>(&pipeline_settings.min_cloud_size)->
+      default_value(pipeline_settings.min_cloud_size),
+      "minimum cloud size; produces an error and ignores clouds smaller than this")
+    ("max-cloud-size", po::value<std::int32_t>(&pipeline_settings.max_cloud_size)->
+      default_value(pipeline_settings.max_cloud_size),
+      "maximum cloud size; produces an error and ignores clouds larger than this");
 
-  // create modules
-  ClientType client(host, port, 100);
-  ParserModuleType parser;
-  ConverterType converter;
+  try
+  {
+    // load the command line options into the variables map
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(description).positional(p).run(), vm);
 
-  CalibrationType calibrator;
+    if (vm.count("help"))
+    {
+      std::cout << description << std::endl;
+      return 0;
+    }
 
-  // setup modules
-  // setFrameId sets the frame id in the PCL point clouds
-  parser.get<PARSER_00_INDEX>().setFrameId("quanergy");
-  // setReturnSelection allows choosing between the 3 returns (Packet00, only)
-  parser.get<PARSER_00_INDEX>().setReturnSelection(0);
-  // setDegreesOfSweepPerCloud allows breaking the point clouds into smaller pieces (M8 only)
-  parser.get<PARSER_00_INDEX>().setDegreesOfSweepPerCloud(360.0);
-  parser.get<PARSER_01_INDEX>().setFrameId("quanergy");
-  parser.get<PARSER_04_INDEX>().setFrameId("quanergy");
+    // if there is a settings file, load that before notifying (which fills the variables)
+    if (vm.count("settings-file"))
+    {
+      std::string settings_file = vm["settings-file"].as<std::string>();
+      quanergy::pipeline::SettingsFileLoader file_loader;
+      file_loader.loadXML(settings_file);
+      pipeline_settings.load(file_loader);
+    }
+
+    // notify; this stores command line options in associated variables
+    po::notify(vm);
+
+    // let the user know if there is no host value
+    if (pipeline_settings.host.empty())
+    {
+      std::cout << "No host provided" << std::endl;
+      std::cout << description << std::endl;
+      return -1;
+    }
+
+    // handle return selection
+    if (!return_string.empty())
+    {
+      pipeline_settings.return_selection = pipeline_settings.returnFromString(return_string);
+    }
+
+    // handle encoder correction parameters
+    if (!correct_params.empty())
+    {
+      if (correct_params.size() == 2)
+      {
+        pipeline_settings.override_encoder_params = true;
+        pipeline_settings.amplitude = correct_params[0];
+        pipeline_settings.phase = correct_params[1];
+      }
+      else
+      {
+        std::cout << "Manual encoder correction expects exactly 2 paramters: amplitude and phase" << std::endl;
+        std::cout << description << std::endl;
+        return -1;
+      }
+    }
+  }
+  catch (po::error& e)
+  {
+    std::cout << "Boost Program Options Error: " << e.what() << std::endl << std::endl;
+    std::cout << description << std::endl;
+    return -1;
+  }
+  catch (std::exception& e)
+  {
+    std::cout << "Error: " << e.what() << std::endl;
+    return -2;
+  }
+
+
+  // create client to get raw packets from the sensor
+  quanergy::client::SensorClient client(pipeline_settings.host, port, 100);
+
+  // create pipeline to produce point cloud from raw packets
+  quanergy::pipeline::SensorPipeline pipeline(pipeline_settings);
 
   // store connections for cleaner shutdown
   std::vector<boost::signals2::connection> connections;
 
-  // connect the packets from the client to the parser
-  connections.push_back(client.connect([&parser](const ClientType::ResultType& pc){ parser.slot(pc); }));
+  ////////////////////////////////////////////
+  /// if you'd like to parse the packets yourself, connect here
+  ////////////////////////////////////////////
+  // connect the packets from the client to the sensor pipeline
+  connections.push_back(client.connect(
+      [&pipeline](const std::shared_ptr<std::vector<char>>& packet){ pipeline.slot(packet); }
+  ));
   
-  if (pcl::console::find_switch(argc, argv, CALIBRATE_STR.c_str()) &&
-      pcl::console::find_switch(argc, argv, MANUAL_CORRECT.c_str()))
-  {
-    std::cout << "Cannot perform calibration and apply manual correction simultaneously." << std::endl;
-    usage(argv);
-    return(0);
-  }
-
-  // if we're doing automatic calibration or if we're setting the calibration
-  // manually, include the calibrator in the chain
-  if (pcl::console::find_switch(argc, argv, CALIBRATE_STR.c_str()) ||
-      pcl::console::find_switch(argc, argv, MANUAL_CORRECT.c_str()))
-  {
-    // connect the parser to the calibrator
-    connections.push_back(parser.connect([&calibrator](const ParserModuleType::ResultType& pc){ calibrator.slot(pc); }));
-    // connect the calibrator to the converter
-    connections.push_back(calibrator.connect([&converter](const CalibrationType::ResultType& pc){ converter.slot(pc); }));
-
-    // set calibrator parameters
-    if (pcl::console::find_switch(argc, argv, MANUAL_CORRECT.c_str()))
-    {
-      if (!pcl::console::find_switch(argc, argv, AMPLITUDE_STR.c_str()) ||
-          !pcl::console::find_switch(argc, argv, PHASE_STR.c_str()))
-      {
-        usage(argv);
-        return(0);
-      }
-      double amplitude = 0.;
-      double phase = 0.;
-      pcl::console::parse_argument(argc, argv, AMPLITUDE_STR.c_str(), amplitude);
-      pcl::console::parse_argument(argc, argv, PHASE_STR.c_str(), phase);
-
-      calibrator.setParams(amplitude, phase);
-    }
-    else if (pcl::console::find_switch(argc, argv, FRAME_RATE_STR.c_str()))
-    {
-      double frame_rate = 10.;
-      pcl::console::parse_argument(argc, argv, FRAME_RATE_STR.c_str(), frame_rate);
-
-      calibrator.setFrameRate(frame_rate);
-    }
-  }
-  else
-  {
-    // connect the parser to the converter
-    connections.push_back(parser.connect([&converter](const ParserModuleType::ResultType& pc){ converter.slot(pc); }));
-  }
-
   ////////////////////////////////////////////
   /// connect application specific logic here to consume the point cloud
   ////////////////////////////////////////////
+  // here we'll simply count the number of packets and output every 100
   unsigned int cloud_count = 0;
-  connections.push_back(converter.connect([&cloud_count](const ConverterType::ResultType& /*pc*/){ ++cloud_count; if(cloud_count % 100 == 0) std::cout << "clouds received: " << cloud_count << std::endl; }));
+  connections.push_back(pipeline.connect(
+      [&cloud_count](const boost::shared_ptr<pcl::PointCloud<quanergy::PointXYZIR>>& /*pc*/)
+      { ++cloud_count; if(cloud_count % 100 == 0) std::cout << "clouds received: " << cloud_count << std::endl; }
+  ));
 
   // variables to help with control flow
   std::mutex state_mutex;
@@ -205,10 +197,10 @@ int main(int argc, char** argv)
 
   while (state != State::EXIT)
   {
-    // get input
     ////////////////////////////////////////
-    /// put application specific logic here
+    /// put application specific logic here related to starting a stopping
     ////////////////////////////////////////
+    // here we'll simply use a CLI
     std::string input;
     std::cout << "Enter 'run' to connect, 'stop' to disconnect, or 'exit' to exit the program" << std::endl;
     std::getline(std::cin, input);
@@ -225,9 +217,9 @@ int main(int argc, char** argv)
         state = State::STOP;
         client.stop();
 
-        if (pcl::console::find_switch(argc, argv, CALIBRATE_STR.c_str()))
+        if (pipeline_settings.calibrate)
         {
-          calibrator.reset();
+          pipeline.encoder_corrector.reset();
         }
       }
       else if (input == "exit")
