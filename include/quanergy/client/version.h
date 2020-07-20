c61df27ad0846a182f61404b0b0fe3e66978254f
Ross Taylor <ross.taylor@quanergy.com>
2020-07-20 12:06:08 -0500

    Ops: bump version

--- include/quanergy/client/version.h
+++ include/quanergy/client/version.h
@@ -12,10 +12,10 @@
 //  QUANERGY_CLIENT_VERSION / 100 % 1000 is the minor version
 //  QUANERGY_CLIENT_VERSION / 100000 is the major version
 
-#define QUANERGY_CLIENT_VERSION 400000
+#define QUANERGY_CLIENT_VERSION 410000
 
 // As a string.
 
-#define  QUANERGY_CLIENT_LIB_VERSION "4.0.0"
+#define  QUANERGY_CLIENT_LIB_VERSION "4.1.0"
 
 #endif
